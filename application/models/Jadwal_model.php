57e2e646e123201b68098ae5b71e7c81b3eb0609
Nursan amar <nursan011@gmail.com>
2018-02-23 04:11:40 +0800

    fix

--- application/models/Jadwal_model.php
+++ application/models/Jadwal_model.php
@@ -9,14 +9,14 @@ class Jadwal_model extends CI_Model
   public function today($id,$hari)
   {
     // $data = $this->db->query("SELECT `matkul`.`nama` AS 'matkul',`dosen`.`nama` AS 'dosen',`kuliah`.`jam`,`kuliah`.`ruangan` FROM mahasiswa INNER JOIN (jadwal INNER JOIN ( (kuliah INNER JOIN matkul ON kuliah.idMatkul=matkul.idMatkul) INNER JOIN dosen ON kuliah.idDosen=dosen.idDosen) ON jadwal.idKuliah=kuliah.idKuliah) ON mahasiswa.idJadwal=jadwal.idJadwal WHERE mahasiswa.nim=".$id." AND kuliah.tanggal='".date('Y-m-d')."'")->result_array();
-    $this->db->select("kuliah.idKuliah,matkul.nama AS 'matkul',dosen.nama AS 'dosen',kuliah.hari,kuliah.jam AS 'time',kuliah.ruangan AS 'room',kuliah.status,kuliah.idTugas",false);
+    $this->db->select('"kuliah.idKuliah","matkul.nama" AS matkul,"dosen.nama" AS dosen,"kuliah.hari","kuliah.jam" AS time,"kuliah.ruangan" AS room,"kuliah.status","kuliah.idTugas"',false);
     $this->db->from('mahasiswa');
     $this->db->join('jadwal','mahasiswa.idJadwal=jadwal.idJadwal','inner');
     $this->db->join('kuliah','jadwal.idKuliah=kuliah.idKuliah','inner');
     $this->db->join('matkul','kuliah.idMatkul=matkul.idMatkul','inner');
     $this->db->join('dosen','kuliah.idDosen=dosen.idDosen','inner');
     $this->db->where('nim',$id);
-    $this->db->where('`kuliah`.`hari`',$hari);
+    $this->db->where('"kuliah.hari"',$hari);
     // $this->db->group_start()
     //   ->from('jadwal')
     //   ->group_start()
